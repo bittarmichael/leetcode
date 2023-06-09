fc4809480d71841cc60b4305cca267f4c430cd02
michael bittar <46082462+bittarmichael@users.noreply.github.com>
2023-06-09 18:52:57 -0500

    Create group_anagrams.py

---
+++ arrays_and_hashing/group_anagrams.py
@@ -0,0 +1,8 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = defaultdict(list)
+        for string in strs:
+            sorted_string = "".join(sorted(string))
+            hashmap[sorted_string].append(string)
+        return hashmap.values()
